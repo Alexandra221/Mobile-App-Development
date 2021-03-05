56ad4f2134d3a5e5fa5fab4773f25883db282fe6
Александра Аброськина <60029457+Alexandra221@users.noreply.github.com>
2021-03-05 19:14:12 +0300

    Add files via upload

---
+++ Activity Work/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Activity_Work
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            MainPage.DisplayAlert("Warning", "Application started", "Fine");
+        }
+
+        protected override void OnSleep()
+        {
+            MainPage.DisplayAlert("Warning", "Application hibernated", "Okay");
+        }
+
+        protected override void OnResume()
+        {
+            MainPage.DisplayAlert("Warning", "Application resumed", "Good");
+        }
+    }
+}

1f1453cf6e251f79ca7f89188a631ad51d1e5ddf
Александра Аброськина <60029457+Alexandra221@users.noreply.github.com>
2021-03-10 20:47:28 +0300

    Add files via upload

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int first = Convert.ToInt32(First.Text);
+                int second = Convert.ToInt32(Second.Text);
+
+                int result = first + second;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Введите числовые данные", "Ввести заново");
+
+            }
+
+
+        }
+    }
+
+}
